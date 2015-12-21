f379372fde7d142d7970c2187a4bf6ee855bb858
shwei83 <j15935741@yahoo.com>
2015-12-21 19:36:43 +0800

    var1

---
+++ src/hw_6/A6.java
@@ -0,0 +1,19 @@
+package hw_6;
+
+public class A6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+class Question {
+	   String description;
+	   public Question(String desc) {
+	      this.description = desc;
+	   }
+	   public void print() {
+	      System.out.println(desc);
+	   }      
+	}
