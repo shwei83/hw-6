baa826a9f8e4805f79a08ac36e7cb16153f09e2a
shwei83 <j15935741@yahoo.com>
2015-12-21 19:53:26 +0800

    印出 happy

--- src/hw_6/A6.java
+++ src/hw_6/A6.java
@@ -1,19 +1,19 @@
 package hw_6;
 
 public class A6 {
-
-	public static void main(String[] args) {
+	public static void main(String[] args) {		
 		// TODO Auto-generated method stub
-
+		Question acct = new Question(null);
+		String desc = "happy";
+		acct.print(desc);
 	}
-
 }
 class Question {
 	   String description;
 	   public Question(String desc) {
 	      this.description = desc;
 	   }
-	   public void print(char[] desc) {
+	   public void print(String desc) {
 	      System.out.println(desc);
 	   }      
 	}
